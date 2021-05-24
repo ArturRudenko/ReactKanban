1cb03224b269bd5894bb5ba06bc4917ee7d9d27b
ArturRudenko <sparksbridge@gmail.com>
2021-05-24 12:00:25 -0700

    updated UiBtn logic

--- src/components/UI/UiBtn/index.js
+++ src/components/UI/UiBtn/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import { Link } from 'react-router-dom'
 
-export default function UiBtn ({ children }) {
-  return (
-    <button className={styles.btn}>
-      {children}
-    </button>
-  )
+export default function UiBtn ({ type = 'button', linkPath, children }) {
+  if (type === 'button') {
+    return (
+      <button className={styles.btn}>
+        {children}
+      </button>
+    )
+  }
+  if (type === 'link') {
+    return (
+      <Link
+        to={linkPath}
+        className={styles.btn}
+      >
+        {children}
+      </Link>
+    )
+  }
 }

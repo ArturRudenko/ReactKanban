42d647f4eb2ffda7f35f1e66e2897f807f501f2e
ArturRudenko <sparksbridge@gmail.com>
2021-05-24 11:51:26 -0700

    added styles to tags page

--- src/views/Tags/index.js
+++ src/views/Tags/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
+import styles from './styles.module.scss'
+import UiBtn from '../../components/UI/UiBtn';
 
 export default function Tags () {
   const tags = useSelector(state => state.tags)
 
   return (
-    <div>
-      <h1>Tags</h1>
-      {tags.map((tag) =>
-        <div key={tag.id}>{tag.title}</div>
-      )}
+    <div className={styles.tagsWrapper}>
+      <ul className={styles.tagList}>
+        {
+          tags.map((tag) =>
+            <li
+              key={tag.id}
+              className={styles.tag}
+            >
+              <span className={styles.tagTitle}>{tag.title}</span>
+              <UiBtn>Update</UiBtn>
+              <span style={{display: 'inline-block', width: '15px'}} />
+              <UiBtn>Remove</UiBtn>
+            </li>
+          )
+        }
+      </ul>
     </div>
   )
 }
